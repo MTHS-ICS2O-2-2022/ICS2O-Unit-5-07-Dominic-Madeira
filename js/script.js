c16ae3c748d3ab8db3d89a8c1c74ad1fddfad32a
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-11 17:20:40 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominic M.
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // input
+  const number = parseInt(document.getElementById('number').value)
+  let answer = 0
+
+  // process
+  for (let counter = 0; counter <= number; counter++ ) {
+  answer += counter
+  }
+  document.getElementById('answer').innerHTML = "The sum of all the numbers from 0 to " + number + " is: " + answer
+}
